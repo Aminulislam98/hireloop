fbd4bf0aad28f5bebff749e5bf1b3e5ece748ae5
Aminul islam <aminurrohman98@gmail.com>
2026-06-02 21:39:01 +0100

    learning RBAC role base access control

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { role } from "better-auth/client";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 client.connect();
@@ -12,5 +13,12 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        default: "seeker",
+      },
+    },
   },
 });
